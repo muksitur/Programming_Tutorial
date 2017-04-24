ba2156389f3d64d3a16ce9eda549387ddb117d23
muksitur <muksitur9191@gmail.com>
2017-04-24 10:27:03 +0200

    Methods updated

--- Methods/src/Circle.java
+++ Methods/src/Circle.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 
 public class Circle {
 	//todo: add some methods to compute a circle's diameter, circumference and area.
 	//print some outputs to confirm you methods are working right.
+	
+	public static double diameter(double r){
+		
+		return 2*r;
+		
+	}
+	
+	public static double circum(double r){
+		
+		return 2*3.1416*r;
+	}
+	
+	public static double area(double r){
+		
+		return 3.1416*r*r;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		Circle cir = new Circle();
+		
+		while (true){
+			
+			System.out.println("Enter the radius: ");
+			
+			double radius = scan.nextDouble();
+			
+			double diameter = cir.diameter(radius);
+			
+			double circum = cir.circum(radius);
+			
+			double area = cir.area(radius);
+			
+			System.out.println("Diameter is: "+diameter+"\nCircumsfarance is: "+circum+"\nArea is: "+area);
+			
+		}
+	}
 }
--- Methods/src/Prime.java
+++ Methods/src/Prime.java
@@ -3,10 +3,47 @@ import java.util.Scanner;
 
 public class Prime {
 	
+	public static int chkprime(int num){
+
+		int temp = 1;
+		int prime_flag = 0;
+		
+		while(temp<=((double)num)/2){
+			
+			temp++;
+			if((num%temp)==0){
+				prime_flag++;
+				System.out.println(num+" is divisible by "+temp);
+			}
+		}
+		
+		return prime_flag;
+		
+	}
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
 		//todo: write a method which tells you if a given number is a prime or not
+		
+		Prime pr = new Prime();
+		
+		while(true){
+		
+			System.out.println("Enter the number: ");
+			
+			int number = scan.nextInt();
+			
+			
+			int flag = pr.chkprime(number);
+			
+			if(flag == 0){
+				System.out.println(number+" is a prime");
+			}
+			else
+				System.out.println(number+" is not a prime");
+				
+		}
 		
 	}
 	
