b17f66ccebbaab0da62567b6a6627f58cb6ca923
muksitur <muksitur9191@gmail.com>
2017-04-26 14:35:36 +0200

    Factorial.java and Fibonacci.java updated

--- Recursion/src/Factorial.java
+++ Recursion/src/Factorial.java
@@ -1,4 +1,35 @@
+import java.util.Scanner;
+
 
 public class Factorial {
-	//todo: implement a recursive functions whicht gives you the factorial value of a given number
+	//todo: implement a recursive functions which gives you the factorial value of a given number
+	
+	public static int factorial(int n){
+		
+		if(n>=1)
+			return n * factorial(n-1);
+		
+		else
+			return 1;
+		
+	}
+	
+	public static void main(String[] args){
+		
+		Scanner scan = new Scanner(System.in);
+		
+		Factorial fac = new Factorial();
+		
+		while(true){
+			
+			System.out.println("Enter a number(n): ");
+			
+			int num = scan.nextInt();
+			
+			int result = fac.factorial(num);
+			
+			System.out.println("n! = "+result);
+			
+		}
+	}
 }
--- Recursion/src/Fibonacci.java
+++ Recursion/src/Fibonacci.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 
 public class Fibonacci {
 	//todo: implement a recursive function which takes an integer value as inputs and outputs the corresponding Fibonacci number.
 	//if you don't know what Fibonacci numbers are you can read it here: https://en.wikipedia.org/wiki/Fibonacci_number
+	
+	
+	public static int fibonacci(int n){
+		
+		if(n>=2){
+			return fibonacci(n-1) + fibonacci(n-2);
+		}
+		
+		else if(n==1)
+			return 1;
+		
+		else
+			return 0;
+	}
+	
+	public static void main(String[] args){
+		
+		Scanner scan = new Scanner(System.in);
+		
+		Fibonacci fib = new Fibonacci();
+		
+		while(true){
+			
+			System.out.println("Enter a number(n): ");
+			
+			int num = scan.nextInt();
+			
+			int result = fib.fibonacci(num);
+			
+			System.out.println("Fibonacci of n = "+result);
+		}
+		
+	}
 }
