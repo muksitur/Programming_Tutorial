37492ead59793f5636b53219e061eb6572668e79
Muhammad Muksitur Rahman <wovu6875@in.uni-weimar.de>
2017-06-07 17:10:06 +0200

    Exception added

--- Exceptions/src/ExceptionHandling.java
+++ Exceptions/src/ExceptionHandling.java
@@ -13,19 +13,54 @@ public class ExceptionHandling {
 		//the exception. Write your own Exception for this case and 
 		//write a method which throws this exception
 
-		int a = 5;
-		int b = 0;
-		int div = a/b;
+		try{
+			int a = 5;
+			int b = 0;
+			int div = a/b;
+		}
+		catch(ArithmeticException e){
+			System.err.println("error occurred");
+			e.printStackTrace();
+		}
 		
-		int[] arr = new int[5];
-		arr[5] = 7;
+		try{
+			int[] arr = new int[5];
+			arr[5] = 7;
+		}
+		catch(ArrayIndexOutOfBoundsException e){
+			System.err.println("error occurred");
+			e.printStackTrace();
+		}
 		
-		String one = "one";
-		int i = Integer.parseInt(one);
+		try{
+			String one = "one";
+			int i = Integer.parseInt(one);
+		}
+		catch(NumberFormatException e){
+			System.err.println("error occurred");
+			e.printStackTrace();
+		}
 		
-		double c = -7.0;
-		double d = Math.sqrt(c);
-		System.out.println(d);
+		try{
+			double c = -7.0;
+			
+			double d = root(c);
+			System.out.println(d);
+		}
+		catch(CustomException e){
+			System.err.println("error occurred");
+			e.printStackTrace();
+		}
+	}
+	
+	public static double root(double c) throws CustomException{
+		if(c<0){
+			throw new CustomException("cannot sqrt negative number");
+		}
+		
+		else{
+			return Math.sqrt(c);
+		}
 	}
 
 }
