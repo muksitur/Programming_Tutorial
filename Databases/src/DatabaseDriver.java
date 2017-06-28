3bf53328aab9b1e203c07e06f5ba42e880c119da
Muhammad Muksitur Rahman <wovu6875@in.uni-weimar.de>
2017-06-28 17:17:40 +0200

    database done

--- Databases/src/DatabaseDriver.java
+++ Databases/src/DatabaseDriver.java
@@ -1,8 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
 
 public class DatabaseDriver {
 	
 	//todo: test your methods from the DatabaseHelper-class here and print the results
 	//note: comment out the whole main-Method of Example.java. Otherwise Eclipse will not compile
 	//as there are two main methods
+	
+	
+	public static void main(String[] args) throws Exception{
+		
+		DatabaseHelper dbh = new DatabaseHelper();
+		
+		dbh.connectdb();
+		dbh.createtable();
+		dbh.insertusers();
+		dbh.insertfriends();
+		dbh.insertmessages();
+		dbh.query1();
+		dbh.query2();
+		dbh.query3();
+		dbh.connclose();
+	}
 
 }
--- Databases/src/DatabaseHelper.java
+++ Databases/src/DatabaseHelper.java
@@ -1,3 +1,10 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class DatabaseHelper {
 
@@ -11,5 +18,138 @@ public class DatabaseHelper {
 	//- get all Nicknames from users
 	//- get the last 5 posted messages from a selected user
 	//- get the last 3 posted messages of all friends of one user
+	
+	static Connection conn;
+	static Statement stat;
+	
+	public static void connectdb() throws Exception{
+		Class.forName("org.sqlite.JDBC");
+        conn = DriverManager.getConnection("jdbc:sqlite:nabil.db");
+	}
+	
+	public static void createtable() throws Exception{
+		stat = conn.createStatement();
+        stat.executeUpdate("drop table if exists people;");
+        
+        stat.executeUpdate("create table Users (nickname UNIQUE, email, password);");
+        stat.executeUpdate("create table Friends (user, friend);");
+        stat.executeUpdate("create table Messages (msg, time, user, friend)");
+	}
+	
+	public static void insertusers() throws Exception{
+		PreparedStatement prep = conn.prepareStatement(
+	            "insert into Users values (?, ?, ?);");
+
+	        prep.setString(1, "Gandhi");
+	        prep.setString(2, "gandhi@gmail.com");
+	        prep.setString(3, "gandhigandhi");
+	        prep.addBatch();
+	        prep.setString(1, "Turing");
+	        prep.setString(2, "turing@gmail.com");
+	        prep.setString(3, "turingturing");
+	        prep.addBatch();
+	        prep.setString(1, "Wittgenstein");
+	        prep.setString(2, "wittgenstein@gmail.com");
+	        prep.setString(3, "brobro");
+	        prep.addBatch();
+	        
+	        conn.setAutoCommit(false);
+	        prep.executeBatch();
+	        conn.setAutoCommit(true);
+	        
+	}
+	
+	public static void insertfriends() throws Exception{
+		PreparedStatement prep = conn.prepareStatement(
+	            "insert into Friends values (?, ?);");
+
+	        prep.setString(1, "Gandhi");
+	        prep.setString(2, "Nehru");
+	        prep.addBatch();
+	        prep.setString(1, "Turing");
+	        prep.setString(2, "Einstein");
+	        prep.addBatch();
+	        prep.setString(1, "Wittgenstein");
+	        prep.setString(2, "Applebee");
+	        prep.addBatch();
+	        
+	        conn.setAutoCommit(false);
+	        prep.executeBatch();
+	        conn.setAutoCommit(true);
+	        
+	}
+	
+	public static void insertmessages() throws Exception{
+		PreparedStatement prep = conn.prepareStatement("insert into Messages values (?, ?, ?, ?);");
+
+	        prep.setString(1, "sup bro");
+	        prep.setLong(2, 11);
+	        prep.setString(3, "Gandhi");
+	        prep.setString(4, "Nehru");
+	        prep.addBatch();
+	        prep.setString(1, "go to hell, bro");
+	        prep.setLong(2, 5);
+	        prep.setString(3, "Gandhi");
+	        prep.setString(4, "Nehru");
+	        prep.addBatch();
+	        prep.setString(1, "hug it out bro");
+	        prep.setString(2, "51");
+	        prep.setString(3, "Gandhi");
+	        prep.setString(4, "Nehru");
+	        prep.addBatch();
+	        prep.setString(1, "ooh yaaa");
+	        prep.setLong(2, 4);
+	        prep.setString(3, "Gandhi");
+	        prep.setString(4, "Nehru");
+	        prep.addBatch();
+	        prep.setString(1, "oooooh yaaaaaaaaa");
+	        prep.setLong(2, 48);
+	        prep.setString(3, "Gandhi");
+	        prep.setString(4, "Nehru");
+	        prep.addBatch();
+	        prep.setString(1, "aaawwwwwesome");
+	        prep.setLong(2, 8);
+	        prep.setString(3, "Gandhi");
+	        prep.setString(4, "Nehru");
+	        prep.addBatch();
+	        
+	        conn.setAutoCommit(false);
+	        prep.executeBatch();
+	        conn.setAutoCommit(true);
+	}
+	
+	public static void query1() throws Exception{
+		ResultSet rs = stat.executeQuery("select nickname from Users;");
+        while (rs.next()) {
+            System.out.println("name = " + rs.getString("nickname"));
+        }
+        rs.close();
+	}
+	
+	public static void query2() throws Exception{
+		PreparedStatement prep = conn.prepareStatement("select msg from Messages where user = ? order by time ASC LIMIT 5");
+		prep.setString(1, "Gandhi");
+		ResultSet rs = prep.executeQuery();
+		
+		while (rs.next()) {
+            System.out.println("message from = " + rs.getString("msg"));
+        }
+        rs.close();
+	}
+	
+	public static void query3() throws Exception{
+		PreparedStatement prep = conn.prepareStatement("select msg from Messages where friend = ? order by time ASC LIMIT 3");
+		prep.setString(1, "Nehru");
+		ResultSet rs = prep.executeQuery();
+		
+		while (rs.next()) {
+            System.out.println("message for = " + rs.getString("msg"));
+        }
+        rs.close();
+	}
+	
+	public static void connclose() throws Exception{
+		conn.close();
+	}
 
 }
